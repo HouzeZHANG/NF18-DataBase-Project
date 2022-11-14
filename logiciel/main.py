ac39592450467e1f1aa8989d302713ca550ce954
Houze Zhang <houze.zhang@etu.utc.fr>
2022-11-14 12:01:08 +0000

    Update main.py

--- logiciel/main.py
+++ logiciel/main.py
@@ -7,16 +7,67 @@ Group number: 05
 conn = None
 
 # variable globale pour indiquer le role de utilisateur
-# si token == 1 il est membrePersonnel, si token == 2 il est aderent
+# si token == 2 il est membrePersonnel, si token == 1 il est aderent
 # mais avant notre utilisateur se connecter, token = None
 role = None
+uid = None
+
+def sqlExecute(sql: string)->list:
+    """cette methode est destine a simplifier le processus de sql, retourner un liste de tuple"""
+    return []
 
 def connect_to_db():
     """return connection of the postresql, if login successfully else return None"""
-    pass
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            host = "",
+            database = "",
+            user = "",
+            password = ""
+        )
+        cur = conn.cursor()
+        print('Postgres version: ')
+        cur.execute('SELECT version()')
+        db_version = cur.fetchone()
+        print(db_version)
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
 
 def login():
     """verification de role de utilisateur"""
+    role = None
+    uid = None
+    
+    print("Vous etes adherent ou membrePersonnel?")
+    print("1: adherent")
+    print("2: membrePersonnel")
+    response = input("Vous etes: 1/2")
+    if response == '1':
+        role = 1
+    elif response == '2':
+        role = 2
+    else:
+        print("input invalide")
+        return
+    
+    print("\nenterez votre username et mot de passe")
+    username = input("username: ")
+    pwd = input("mot de passe: ")
+    
+    if role == 1:
+        sql = """
+        
+        """
+    else:
+        sql = """
+        
+        """
     pass
 
 def loop(conn):
@@ -72,8 +123,10 @@ def gestionDesSactions():
 
 if __name__ == "__main__":
     conn = connect_to_db()
-    uid = login()
-    while conn is not None:
-        loop(conn)
-
-    print("Connextion not exists")
+    if conn is None:
+        pass
+    else:
+        while uid is None:
+            login()
+        while conn is not None:
+            loop(conn)
